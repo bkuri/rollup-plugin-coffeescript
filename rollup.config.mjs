9bcd9be3d937dbfa0d39542821a5e92356bc40aa
Bernardo Kuri <github@bkuri.com>
2023-09-21 10:21:49 -0600

    refactor plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,29 +1,35 @@
-import { createRequire } from 'node:module'
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+import pkg from './package.json' assert { type: 'json' }
 
-const require = createRequire(import.meta.url)
-const pkg = require('./package.json')
+const globals = {
+  coffeescript: 'coffeescript',
+  path: 'path',
+  picomatch: 'pm'
+}
 
 export default [
-  {
-    input: 'src/index.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'rollup-plugin-coffeescript'
-    },
-		plugins: [
-			resolve(),
-			commonjs()
-		]
-  },
   {
     external: [ 'coffeescript' ],
     input: 'src/index.js',
     output: [
       { file: pkg.main  , format: 'cjs' },
-      { file: pkg.module, format: 'es'  }
+      { file: pkg.module, format: 'esm' }
+    ],
+    plugins: [
+      nodeResolve({ resolveOnly: [ '@rollup/pluginutils' ] })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.browser, format: 'umd', globals, name: 'rollup-plugin-coffeescript' }
+    ],
+    plugins: [
+      commonjs(),
+      nodePolyfills({ include: [ 'path' ] }),
+      nodeResolve()
     ]
   }
 ]
